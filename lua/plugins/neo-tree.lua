0fd2b063c7203c81473c76455d89b93e6ba40ebb
KAMRON-DEVELOPER <atajanovkamronbek2003@gmail.com>
2024-10-23 18:12:45 +0500

    last

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -32,10 +32,21 @@ return {
 		keymap.set("n", "<C-p>", ":Neotree buffers reveal float<CR>", { desc = "Neotree buffers reveal float" })
 
 		-- If you want icons for diagnostic errors, you'll need to define them somewhere:
-		vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
-		vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
-		vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
-		vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
+		-- Diagnostika xatolari uchun belgilarni sozlash
+		vim.diagnostic.config({
+			signs = {
+				error = { text = " ", texthl = "DiagnosticSignError" },
+				warn = { text = " ", texthl = "DiagnosticSignWarn" },
+				info = { text = " ", texthl = "DiagnosticSignInfo" },
+				hint = { text = "󰌵", texthl = "DiagnosticSignHint" },
+			},
+		})
+
+		-- depricated!!!
+		-- vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
+		-- vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
+		-- vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
+		-- vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
 
 		require("neo-tree").setup({
 			close_if_last_window = true,
