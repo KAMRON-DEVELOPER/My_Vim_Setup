ec618724bc7b4f6e35e6a65ea0a93a62d811dfbe
KAMRON-DEVELOPER <atajanovkamronbek2003@gmail.com>
2024-10-28 15:33:38 +0500

    fugitive

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,81 +1,26 @@
 return {
-	{
-		"tpope/vim-fugitive",
-	},
-	{
-		"lewis6991/gitsigns.nvim",
-		config = function()
-			local gitsigns = require("gitsigns")
+	"tpope/vim-fugitive",
 
-			gitsigns.setup({
-				signs = {
-					add = { text = "┃" },
-					change = { text = "┃" },
-					delete = { text = "_" },
-					topdelete = { text = "‾" },
-					changedelete = { text = "~" },
-					untracked = { text = "┆" },
-				},
-				signs_staged = {
-					add = { text = "┃" },
-					change = { text = "┃" },
-					delete = { text = "_" },
-					topdelete = { text = "‾" },
-					changedelete = { text = "~" },
-					untracked = { text = "┆" },
-				},
-				signs_staged_enable = true, -- Toggle with `:Gitsigns toggle_signs`
-				signcolumn = true,
-				current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
-				current_line_blame_formatter = "<author>, <author_time:%R> - <summary>",
+	config = function()
+		local keymap = vim.keymap
 
-				on_attach = function(bufnr)
-					--set keymap
-					local keymap = vim.keymap
-
-					-- Helper function to map keys
-					local function map(mode, lhs, rhs, opts)
-						opts = opts or {}
-						opts.buffer = bufnr
-						keymap.set(mode, lhs, rhs, opts)
-					end
-
-					-- Navigation between hunks
-					map("n", "]c", function()
-						if vim.wo.diff then
-							vim.cmd.normal({ "]c", bang = true })
-						else
-							gitsigns.nav_hunk("next")
-						end
-					end)
-
-					map("n", "[c", function()
-						if vim.wo.diff then
-							vim.cmd.normal({ "[c", bang = true })
-						else
-							gitsigns.nav_hunk("prev")
-						end
-					end)
-
-					keymap.set("n", "<leader>gs", ":Gitsigns stage_hunk<CR>", { desc = "Gitsigns stage_hunk" })
-					keymap.set("n", "<leader>gr", ":Gitsigns reset_hunk<CR>", { desc = "Gitsigns reset_hunk" })
-
-					-- Visual mode for staging/resetting specific lines
-					keymap.set("v", "<leader>gs", function()gitsigns.stage_hunk(vim.fn.line("."), vim.fn.line("v")) end, { desc = "Gitsigns stage selected hunk" })
-					keymap.set("v", "<leader>gr", function() gitsigns.reset_hunk(vim.fn.line("."), vim.fn.line("v")) end, { desc = "Gitsigns reset selected hunk" })
-
-					-- Additional Gitsigns key bindings
-					keymap.set("n", "<leader>gS", ":Gitsigns stage_buffer<CR>", { desc = "Gitsigns stage_buffer" })
-					keymap.set("n","<leader>gu", ":Gitsigns undo_stage_hunk<CR>", { desc = "Gitsigns undo_stage_hunk" })
-					keymap.set("n", "<leader>gR", ":Gitsigns reset_buffer<CR>", { desc = "Gitsigns reset_buffer" })
-					keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", { desc = "Gitsigns preview_hunk" })
-					keymap.set("n", "<leader>gb", function() gitsigns.blame_line({ full = true }) end, { desc = "Gitsigns blame line with full info" })
-					keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", { desc = "Gitsigns toggle_current_line_blame" })
-					keymap.set("n", "<leader>gd", ":Gitsigns diffthis<CR>", { desc = "Gitsigns diffthis" })
-					keymap.set("n", "<leader>gD", function() gitsigns.diffthis("~") end, { desc = "Gitsigns show diff with previous commit" })
-					keymap.set("n", "<leader>gT", ":Gitsigns toggle_deleted<CR>", { desc = "Gitsigns toggle_deleted" })
-				end,
-			})
-		end,
-	},
+		keymap.set("n", "<leader>gs", ":Git<CR>", { noremap = true, silent = true, desc = "Git status" })
+		keymap.set("n", "<leader>gd", ":Git diff<CR>", { noremap = true, silent = true, desc = "Git giff" })
+		keymap.set("n", "<leader>gaa", ":Git add .<CR>", { noremap = true, silent = true, desc = "Git add all" })
+		keymap.set("n", "<leader>gaf", function()
+			vim.ui.input({ prompt = "Enter file to git add: " }, function(input)
+				if input then
+					vim.cmd("Git add " .. input)
+				end
+			end)
+		end, { noremap = true, silent = true, desc = "Git add file" })
+		keymap.set("n", "<leader>gc", function()
+			vim.ui.input({ prompt = "Enter commit message: " }, function(input)
+				if input then
+					vim.cmd('Git commit -m "' .. input .. '"')
+				end
+			end)
+		end, { noremap = true, silent = true, desc = "Git commit" })
+		keymap.set("n", "<leader>gp", ":Git push<CR>", { noremap = true, silent = true, desc = "Git push" })
+	end,
 }
